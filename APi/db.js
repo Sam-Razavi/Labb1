d5105695ccf1552e6c032bf903faa225502ca3ad
Sam <sam.sahbaei.razavi@iths.se>
2023-05-16 00:59:38 +0200

    2

--- APi/db.js
+++ APi/db.js
@@ -2,7 +2,8 @@ const Pool = require('pg').Pool;
 
 
 const pool = new Pool({
-    user : 'admin',
+    user : 'postgres',
+    password : 'spider7687072',
     host : 'localhost',
     database : 'book',
     port : 5432,
